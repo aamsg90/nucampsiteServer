c09a465b78e0fc962666916370af9ed36d53b0bf
aamsg90 <75170318+aamsg90@users.noreply.github.com>
2022-04-24 12:18:45 -0400

    workshop4

--- authenticate.js
+++ authenticate.js
@@ -1,82 +1,94 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+//import PASSPORT
+const passport = require("passport");
+//import PASSPORT-LOCAL & set it as the AUTHENTICATION STRATEGY to store unames/pwords locally on SERVER-SIDE
+const LocalStrategy = require("passport-local").Strategy;
 const FacebookTokenStrategy = require('passport-facebook-token');
+//import our User SCHEMA
+const User = require("./models/user");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
 
-const config = require('./config.js');
-
+const config = require("./config.js");
+//creates new INSTANCE of LocalStrategy, requires a verifyCallback FUNCTION(.authenticate() from PASSPORT-LOCAL)
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
+
+//set up SESSION based AUTHENTICATION using SERIALIZATION/DE-SERIALIZATION of User INSTANCE
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+exports.getToken = (user) => {
+  //returns USER, .secretKey IMPORTED from 'config.js', & CONFIGS to expire TOKEN
+  return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
+
+
 exports.jwtPassport = passport.use(
-    new JwtStrategy(
-        opts,
-        (jwt_payload, done) => {
-            console.log('JWT payload:', jwt_payload);
-            User.findOne({_id: jwt_payload._id}, (err, user) => {
-                if (err) {
-                    return done(err, false);
-                } else if (user) {
-                    return done(null, user);
-                } else {
-                    return done(null, false);
-                }
-            });
-        }
-    )
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log('JWT payload:' ,jwt_payload);
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
+      if (err) {
+        return done(err, false);
+      } else if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
 );
 
-exports.facebookPassport = passport.use(
-    new FacebookTokenStrategy(
-        {
-            clientID: config.facebook.clientId,
-            clientSecret: config.facebook.clientSecret
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({facebookId: profile.id}, (err, user) => {
-                if (err) {
-                    return done(err, false);
-                }
-                if (!err && user) {
-                    return done(null, user);
-                } else {
-                    user = new User({ username: profile.displayName });
-                    user.facebookId = profile.id;
-                    user.firstname = profile.name.givenName;
-                    user.lastname = profile.name.familyName;
-                    user.save((err, user) => {
-                        if (err) {
-                            return done(err, false);
-                        } else {
-                            return done(null, user);
-                        }
-                    });
-                }
-            });
-        }
-    )
-);
+
+// exports.verifyAdmin = passport.authenticate('jwt', {session: false});
+// exports.facebookPassport = passport.use(
+//     new FacebookTokenStrategy(
+//         {
+//             clientID: config.facebook.clientId,
+//             clientSecret: config.facebook.clientSecret
+//         }, 
+//         (accessToken, refreshToken, profile, done) => {
+//             User.findOne({facebookId: profile.id}, (err, user) => {
+//                 if (err) {
+//                     return done(err, false);
+//                 }
+//                 if (!err && user) {
+//                     return done(null, user);
+//                 } else {
+//                     user = new User({ username: profile.displayName });
+//                     user.facebookId = profile.id;
+//                     user.firstname = profile.name.givenName;
+//                     user.lastname = profile.name.familyName;
+//                     user.save((err, user) => {
+//                         if (err) {
+//                             return done(err, false);
+//                         } else {
+//                             return done(null, user);
+//                         }
+//                     });
+//                 }
+//             });
+//         }
+//     )
+// );
+
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
-exports.verifyAdmin = function(req, res, next) {
-    if(req.user.admin === true) {
-        return next();
-    } else {
-        const err = new Error("You are not authorized to perform this operation!");
-        err.status = 403;
-        return next(err);
-    }
-};
+exports.verifyAdmin = (req, res, next) => {
+    if (!req.user.admin) {
+      // res.statusCode = 200;
+      // res.setHeader('Content-Type', 'application/json')
+      // res.json(req.headers)
+      err = new Error('You are not an admin')
+      err.status = 403;
+      
+      return next(err);
+    } 
+      return next();
+    
+  }
--- routes/users.js
+++ routes/users.js
@@ -1,82 +1,153 @@
-const express = require('express');
-const User = require('../models/user');
-const passport = require('passport');
-const authenticate = require('../authenticate');
+const express = require("express");
+//import our 'user' MONGOOSE SCHEMA
+const User = require("../models/user");
+const passport = require("passport");
+const authenticate = require("../authenticate");
 const cors = require('./cors');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    User.find()
-    .then(users => {
+router.get("/", cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find()
+      .then((users) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(users);
-    })
-    .catch(err => next(err));
+      })
+      // res.end("Will send all the campsites to you.");
+      .catch((err) => next(err));
 });
 
-router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
-    if (req.user) {
-        const token = authenticate.getToken({_id: req.user._id});
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, token: token, status: 'You are successfully logged in!'});
-    }
-});
-
-router.post('/signup', cors.corsWithOptions,  (req, res) => {
-    User.register(
-        new User({username: req.body.username}),
-        req.body.password,
-        (err, user) => {
-            if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({err: err});
-            } else {
-                if (req.body.firstname) {
-                    user.firstname = req.body.firstname;
-                }
-                if (req.body.lastname) {
-                    user.lastname = req.body.lastname;
-                }
-                user.save(err => {
-                    if (err) {
-                        res.statusCode = 500;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json({err: err});
-                        return;
-                    }
-                    passport.authenticate('local')(req, res, () => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json({success: true, status: 'Registration Successful!'});
-                    });
-                });
-            }
+router.post("/signup", cors.corsWithOptions, (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        //handles the event of a DB error
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ err: err });
+      } else {
+        //authenticate with PASSPORT
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
         }
-    );
-});
-
-router.post('/login', cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
-    const token = authenticate.getToken({_id: req.user._id});
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
-
-router.get('/logout', cors.corsWithOptions, (req, res, next) => {
-    if (req.session) {
-        req.session.destroy();
-        res.clearCookie('session-id');
-        res.redirect('/');
-    } else {
-        const err = new Error('You are not logged in!');
-        err.status = 401;
-        return next(err);
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save(err => {
+          if (err) {
+            res.statusCode = 500;
+             res.setHeader("Content-Type", "application/json");
+            res.json({err:err});
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
+        })
+        
+      }
     }
+  );
 });
+//*** re-wrote this COOKIES/SESSION-BASED auth function to use PASSPORT ^^^^^*/
+// User.findOne({ username: req.body.username })
+//   .then((user) => {
+//     if (user) {
+//       const err = new Error(`User ${req.body.username} already exists.`);
+//       err.status = 403;
+//       return next(err);
+//     } else {
+//       User.create({
+//         username: req.body.username,
+//         password: req.body.password,
+//       })
+//         .then((user) => {
+//           res.statusCode = 200;
+//           res.setHeader("Content-Type", "application/json");
+//           res.json({ status: "Registration successful", user: user });
+//         })
+//         .catch((err) => next(err));
+//     }
+//   })
+//   .catch((err) => next(err));
+
+//add the authenticate METHOD from PASSPORT
+router.post("/login", cors.corsWithOptions, passport.authenticate("local"), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    success: true,
+    token: token,
+    status: "You are successfully logged in!",
+  });
+});
+//****re-write COOKIES/SESSION-BASED auth with PASSPORT-LOCAL, which will handle checking the username/password combo and handle any errors ^^^^^^^ */
+//   if (!req.session.user) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//       const err = new Error("You are not authenticated!");
+//       res.setHeader("WWW-Authenticate", "Basic");
+//       err.status = 401;
+//       return next(err);
+//     }
+
+//     const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+//       .toString()
+//       .split(":");
+//     const username = auth[0];
+//     const password = auth[1];
+
+//     //Check the username and password that the USER/CLIENT is sending and check it against the DOCUMENTS in the 'Users' mongoDB COLLECTION. 1) find a USER in the DB that matches the 'username' being sent to us as part of the CREDENTIALS in the REQUEST HEADERS vvv  2)checking the 'username' field set up by our 'user'Schema bc it must be unique
+//     User.findOne({ username: username })
+//       .then((user) => {
+//         if (!user) {
+//           const err = new Error(`User ${username} does not exist.`);
+//           err.status = 401;
+//           return next(err);
+//         } else if (user.password !== password) {
+//           const err = new Error(`Password does not match ${username}.`);
+//           err.status = 401;
+//           return next(err);
+//         } else if (user.username === username && user.password === password) {
+//           req.session.user = "authenticated";
+//           res.statusCode = 200;
+//           res.setHeader("Content-Type", "text/plain");
+//           res.end("You are authenticated.");
+//         }
+//       })
+//       .catch((err) => next(err));
+//   } else {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("You are already authenticated.");
+//   }
+// });
+
+router.get("/logout", cors.corsWithOptions, (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    const err = new Error("You are not logged in.");
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+//     if (req.user) {
+//         const token = authenticate.getToken({_id: req.user._id});
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         res.json({success: true, token: token, status: 'You are successfully logged in!'});
+//     }
+// });
 
 module.exports = router;
